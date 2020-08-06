48981e03d8d038e3b58c41bf5274092f4f2647f9
Jacksiesta <jacqueline.herrald@gmail.com>
2020-08-06 17:22:16 +0200

    30 fkg lines

--- squrt.c
+++ squrt.c
@@ -4,35 +4,33 @@ double		ft_squrt(int nb, int p)
 {
 	int 	left;
 	int 	right;
-	int 	mid;
+	float 	mid;
 	float 	res;
-	float 	incr;
 
 	right = nb;
+	left = 0;
 	while (left <= right)
 	{
 		mid = (left + right) / 2;
-		if (mid * mid == nb)
+		if (mid * mid == nb || mid * mid < nb)
 		{
 			res = mid;
-			break; 
-		}
-		if (mid * mid < nb)
-		{
-			left = mid + 1;
-			res = mid;
+			if (mid * mid == nb)
+				break; 
+			else
+				left = mid + 1;
 		}
 		else
 			right = mid - 1;
 	}
 	left = 0;
-	incr = 0.1;
+	mid = 0.1;
 	while (left++ < p)
 	{
 		while (res * res <= nb)
-			res += incr;
-		res -= incr;
-		incr /= 10;
+			res += mid;
+		res -= mid;
+		mid /= 10;
 	}
 	return (res);
 }
