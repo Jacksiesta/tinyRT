fd6bb58a8737e5fe74e27594ed94efa9c9bf5116
becentrale <becentrale@w-r2-p1.s19.be>
2020-08-14 16:53:09 +0200

    includes sort out

--- includes/miniRT.h
+++ includes/miniRT.h
@@ -10,18 +10,19 @@
 #include "mlx.h"
 #include "sphere.h"
 
+/*
 typedef struct	s_sphere
 {
 	t_pos 	*center;
 	float 	radius;
 	int	color;
-}		t_sphere;
+}		t_sphere;*/
 
 t_lstobject	*create_obj(int t, void *o);
 t_pos		*create_pos(double x, double y, double z);
 t_pos2d		*create_pos2d(int x, int y);
 void		set_pos(t_pos *position, double x, double y, double z);
-t_sphere	*create_sphere(int radius, int color);
+//t_sphere	*create_sphere(int radius, int color);
 t_canvas	*create_canvas(int w, int h, int d);
 int		dot_product(t_pos pos1, t_pos pos2);
 t_pos		vectorSub(t_pos *v1, t_pos *v2);
---
+++ includes/sphere.h
@@ -0,0 +1,15 @@
+#ifndef SPHERE_H
+# define SPHERE_H
+
+#include <stdlib.h>
+
+typedef struct	s_sphere
+{
+	t_pos 	*center;
+	float 	radius;
+	int	color;
+}		t_sphere;
+
+t_sphere	*create_sphere(int radius, int color);
+
+#endif
