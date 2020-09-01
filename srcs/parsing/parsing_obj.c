49800cd807937031c7220dc109597d149cc99b9f
hde-ghel <jacqueline.herrald@gmail.com>
2020-09-01 18:01:49 +0200

    few lines of sphere_parser

--- srcs/parsing/parsing_obj.c
+++ srcs/parsing/parsing_obj.c
@@ -1,7 +1,15 @@
 #include "../../includes/miniRT.h"
-i/*
+
 int	parsing_sphere(t_scene **scene, char *line)
 {
-		
+	t_vector	*center;
+	float		radius;
+	int		color;
+	int		x[3];
 	
-}*/
+	x[0] = 2;
+	x[1] = -1;
+	x[2] = 0;
+	while (++x[1] < 4 && ())		
+	
+}
